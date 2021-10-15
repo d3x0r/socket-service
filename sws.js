9006037aa3095b2580fd490533df7eb7b644ddef
d3x0r <d3x0r@no-reply.github.com>
2021-10-15 03:11:50 -0700

    Fix/improve service worker service interface

--- sws.js
+++ sws.js
@@ -4,12 +4,22 @@ import {sack} from "sack.vfs" ;
 const disk = sack.Volume();
 
 
-export function handleRequest( req ) {
+export function handleRequest( req, res ) {
+       	const contentType = 'text/javascript';
 	let filePath = req.url;
 	if( req.url === '/socket-service-swbundle.js' ) filePath = 'node_modules/@d3x0r/socket-service/swbundle.js'
 	else if( req.url === '/socket-service-client.js' ) filePath = 'node_modules/@d3x0r/socket-service/swc.js'
+	else if( req.url === '/login/webSocketClient.js' ) {
+		const file = sack.HTTPS.get( {method:"GET", port:8089, path:"/ui"+req.url, hostname:"d3x0r.org" } );
+		if( file.error ) {
+			console.log( "REspond error:", file );
+			return true;
+		}
+		res.writeHead(200, { 'Content-Type': contentType });
+		res.end( file.content );
+		return true;
+	}
 	else return false;
-       	contentType = 'text/javascript';
 
 	if( disk.exists( filePath ) ) {
 		res.writeHead(200, { 'Content-Type': contentType });
