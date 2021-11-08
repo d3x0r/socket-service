33857b372ae860ead3ac59934ae060e2d1724a3b
d3x0r <d3x0r@no-reply.github.com>
2021-11-07 16:01:28 -0800

    Remove internal redirects

--- sws.js
+++ sws.js
@@ -7,8 +7,10 @@ const disk = sack.Volume();
 export function handleRequest( req, res ) {
        	const contentType = 'text/javascript';
 	let filePath = req.url;
+	// redirect this file's source so it can serve root content.
 	if( req.url === '/socket-service-swbundle.js' ) filePath = 'node_modules/@d3x0r/socket-service/swbundle.js'
 	else if( req.url === '/socket-service-client.js' ) filePath = 'node_modules/@d3x0r/socket-service/swc.js'
+/*
 	else if( req.url === '/login/webSocketClient.js' ) {
 		const file = sack.HTTPS.get( {method:"GET", port:8089, path:"/ui"+req.url, hostname:"d3x0r.org" } );
 		if( file.error ) {
@@ -19,6 +21,7 @@ export function handleRequest( req, res ) {
 		res.end( file.content );
 		return true;
 	}
+*/
 	else return false;
 
 	if( disk.exists( filePath ) ) {
